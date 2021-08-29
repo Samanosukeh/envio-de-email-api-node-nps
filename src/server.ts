ee9196e02908a804c9bee3a06f3ccd3134dbd683
Samanosukeh <flauberth_94@hotmail.com>
2021-08-28 23:01:19 -0300

    métodos de exemplo: GET e POST construido

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,20 @@ import express from 'express';
 //tente instalar a depedencia de tipagem do express: npm i --save-dev @types/express só no desenvolvimento
 const app = express();
 
-app.listen(3333, () => console.log("Server is running!"));
+const PORT = 3333;
+
+app.get("/", (request, response) => {
+    //return response.send("Hello world");print
+    return response.json({message: "Hello world"});
+});
+
+// 1 parâmetro: rota,
+// 1 parâmetro: request, response
+app.post("/",  (request, response) => {
+    // recebendo os dados
+    return response.json({message: "Os dados foram salvos com sucesso!"});
+});
+
+app.listen(PORT, () => console.log("Server is running!"));
 // node precisa de um tradutor para o typescript, instalar o typescript: npm add typescript -D
 // iniciando o typescript dentro da aplicação: npm run tsc -- --init 
