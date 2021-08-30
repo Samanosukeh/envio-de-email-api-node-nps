c93c50ee803e14eccfd2368956c5c35a12882f4b
Samanosukeh <flauberth_94@hotmail.com>
2021-08-30 15:17:01 -0300

    - Refatorado controller - criado repositorio de usuário

--- src/controllers/UserControllers.ts
+++ src/controllers/UserControllers.ts
@@ -1,12 +1,12 @@
 import { Request, Response } from "express";
-import { getRepository } from "typeorm";
-import { User } from "../models/User";
+import { getCustomRepository } from "typeorm";
+import { UsersRepository } from "../repositories/UsersRepository";
 
 class UserController {
     async create(request: Request, response: Response) {
         const { name, email } = request.body;// recebendo apenas o nome e e-mail passados pelo json
         
-        const usersRepository = getRepository(User);//passando a entidade que quero criar
+        const usersRepository = getCustomRepository(UsersRepository);//passando a entidade que quero criar
 
         // select * from users where email="email" :)
         const userAlreadyExists = await usersRepository.findOne({email});
@@ -25,4 +25,4 @@ class UserController {
     }
 }
 
-export { UserController }
+export { UserController };
